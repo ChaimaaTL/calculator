7f41a09c4ed42ca41441efdb7ad210d3bcf3d0ba
ChaimaaTL <chaimaa.tawil7@gmail.com>
2023-12-01 10:07:56 +0100

    Ajouter le squelette Spring Boot

---
+++ src/test/java/com/tawil/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tawil.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
